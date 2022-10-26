957b2fbfddd768d44c4574d47737d74af9344b67
vishal1810051 <vishal1810051@gmail.com>
2022-10-26 12:38:17 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import HemisphereDisplay from './HemisphereDisplay';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+
+    state = { latitude: null, error: " " }
+    componentDidMount() {
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => { this.setState({ latitude: position.coords.latitude, longitude: position.coords.longitude }) },
+            (error) => { this.setState({ error: error.message }) }
+        );
+    }
+
+    render() {
+        if (this.state.error && !this.state.latitude) {
+            return this.state.error
+        }
+        else {
+            return <div><HemisphereDisplay latitude={this.state.latitude} /></div>
+        }
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+)
+
+
+
